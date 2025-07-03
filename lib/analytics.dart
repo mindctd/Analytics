229d6bf3ee768f244a969c2e54f2d4f3a5ae45de
mindctd <134094358+mindctd@users.noreply.github.com>
2025-07-03 10:36:36 +0700

    Fix analytics version 2

--- lib/analytics.dart
+++ lib/analytics.dart
@@ -1,17 +1,17 @@
-// import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-  // final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+   MyHomePage({super.key});
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
           child: ElevatedButton(
               onPressed: () {
-                // analytics.logEvent(name: "Test", parameters: {"int": 40});
-                // print('sended event');
+                analytics.logEvent(name: "Test", parameters: {"int": 40});
+                
               },
               child: const Text("Test Analytic kub Pe chai ja ja"))),
     );
